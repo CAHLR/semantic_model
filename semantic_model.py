63dc60027c712c27ffc971141613de2d894e765d
Tiffany Jann <tiffanyjann@berkeley.edu>
2017-12-29 14:35:51 -0800

    limit CountVectorizer vocab, recompute vocab and coeffs every time, BOW in separate file, txt files for gzip

--- semantic_model.py
+++ semantic_model.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import getopt
 import time
@@ -83,9 +84,10 @@ def get_vocab(dataframe, column):
     from nltk.tokenize import word_tokenize
 
     dataframe[column] = dataframe[column].fillna('')
-    vectorizer = CountVectorizer(stop_words='english', ngram_range=(1,1))
+    vectorizer = CountVectorizer(stop_words='english', ngram_range=(1,1), max_features=2500)
     X = vectorizer.fit_transform(dataframe[column])
     unigrams = vectorizer.get_feature_names()
+    print('Taking the minimum of 2500 and ' + len(unigrams) + ' unigrams')
     vectorizer = CountVectorizer(stop_words='english', ngram_range=(2,2), max_features=int(len(unigrams)/10))
     X = vectorizer.fit_transform(dataframe[column])
     bigrams = vectorizer.get_feature_names()
@@ -135,36 +137,34 @@ if (len_vec_frame != len_raw_frame):
     sys.exit()
 
 if (blobcolumn != ''):
-    try:
-        vocab_frame = read_big_csv(outputfile+'_vocab.tsv')
-        vocab = vocab_frame.values.flatten()
-    except:
-        vocab = get_vocab(raw_frame, blobcolumn)
-        vocab_frame = pd.DataFrame(vocab)
+    # Recompute vocab every time, just in case.
+    vocab = get_vocab(raw_frame, blobcolumn)
+    vocab_frame = pd.DataFrame(vocab)
     vocabsize = len(vocab)
     X = to_bag_of_words(raw_frame, blobcolumn, vocab)
     M = X.toarray()
-    try:
-        weights_frame = read_big_csv(outputfile+'_weights.tsv')
-        biases = read_big_csv(outputfile+'_biases.tsv').iloc[:,0]
-    except:
-        (weights_frame, biases) = logistic_regression(vec_frame.iloc[:,1:], M)
-    vectors_frame = vec_frame.iloc[:,1:]
-    result_frame = vectors_frame.dot(weights_frame.values)
+    # Recompute coefficients every time, just in case.
+    (weights_frame, biases) = logistic_regression(vec_frame.iloc[:,1:], M)
+    result_frame = vec_frame.iloc[:,1:].dot(weights_frame.values)
     result_frame += biases
     print('Sorting classification results...')
     sorted_frame = np.argsort(result_frame,axis=1).iloc[:,-num_top_words:]
-
     for i in range(num_top_words):
         new_col = vocab_frame.iloc[sorted_frame.iloc[:,i],0] # get the ith top vocab word for each entry
         raw_frame['predicted_word_' + str(num_top_words-i)] = new_col.values
 
-    vec_frame['bow'] = list(M)
-    raw_frame['bow'] =  list(M)
+    bow_frame = pd.DataFrame(M)
 
 print('Writing results to file...')
 raw_frame.to_csv(outputfile+'.tsv', sep = '\t', index = False)
-vec_frame.to_csv(re.split("\.[a-z]{1,4}", vectorfile)[0]+'_with_bow.tsv', sep = '\t', index = False)
-vectors_frame.to_csv(outputfile+'_vectors.tsv', sep = '\t', index = False)
+bow_frame.to_csv(outputfile+'_bow.tsv', sep = '\t', index = False)
+
+print('Copying tsv files to txt for gzip')
+os.system('cp '+ outputfile+'_weights.tsv' + ' ' + outputfile+'_weights.txt')
+os.system('cp '+ outputfile+'_biases.tsv' + ' ' + outputfile+'_biases.txt')
+os.system('cp '+ outputfile + '_vocab.tsv' + ' ' + outputfile + '_vocab.txt')
+os.system('cp '+ outputfile+'.tsv' + ' ' + outputfile+'.txt')
+os.system('cp '+ outputfile+'_bow.tsv' + ' ' + outputfile+'_bow.txt')
+os.system('cp '+ vectorfile + ' ' + re.split(".tsv", vectorfile)[0]+'.txt')
 timeaf = time.time()
 print('TIME: ',timeaf-timebf)
